25ad8cb3a4a4326b95d476ca88f33e54b3017de8
zvetaM <matevz.markovic@xlab.si>
2016-05-25 14:13:11 +0200

    Update drupal_update.sh
    Vključim še drush make distro.make, ki namesti stran v /var/www/drupal/dgu

--- drupal_update.sh
+++ drupal_update.sh
@@ -18,6 +18,7 @@ sudo -u co bash -c "source /home/co/.bash_profile ; cd /src/ckanext-dgu ; grunt"
 sudo -u co bash -c "source /home/co/.bash_profile ; cd /src/shared_dguk_assets ; sudo npm install" || echo "******NAPAKA****** sudo npm install v /src/shared_dguk_assets ni uspel"
 sudo -u co bash -c "source /home/co/.bash_profile ; cd /src/shared_dguk_assets ; grunt" || echo "******NAPAKA****** grunt v /src/shared_dguk_assets ni uspel"
 
+sudo -u co bash -c "source /home/co/.bash_profile ; cd /src ; drush make /src/dgu_d7/distro.make /var/www/drupal/dgu" || echo "******NAPAKA****** drush make distro.make ni uspel"
 sudo -u co bash -c "source /home/co/.bash_profile ; cd /var/www/drupal/dgu ; drush --yes --verbose site-install dgu --db-url=mysql://co:pass@localhost/dgu --account-name=admin --account-pass=admin  --site-name='Portal odprtih podatkov Slovenije'" || echo "******NAPAKA****** drush site-install ni uspel"
 sudo -u co bash -c "source /home/co/.bash_profile ; cd /var/www/drupal/dgu ; drush --yes en dgu_app dgu_blog dgu_consultation dgu_data_set dgu_data_set_request dgu_footer dgu_forum dgu_glossary dgu_idea dgu_library dgu_linked_data dgu_location dgu_moderation dgu_notifications dgu_organogram dgu_print dgu_reply dgu_search dgu_services dgu_user ckan" || echo "******NAPAKA****** drush module install ni uspel"
 
